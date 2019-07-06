3971139f8c9d708dea1f53e98388e1c1162a3adf
haroldo <haroldoop@gmail.com>
2019-07-05 22:21:19 -0300

    Added JS block, but it is destroying the remaining content

--- src/twison.js
+++ src/twison.js
@@ -26,13 +26,74 @@ var Twison = {
   
   extractCommandsFromText: function(text) {
     var lines = text.split(/[\r\n]+/g)
+      // Removes links and trailing spaces
       .map(function(s){ 
         return s.replace(/\[\[.*?\]\]/g, '').trimEnd();
       })
+      // Removes empty lines and comments
       .filter(function(s) {
-        return s && s !== '&lt;p&gt;' && !s.startsWith('//');
+        return s && !s.startsWith('//');
       });
-    return lines;
+    
+    var commands = Twison.processScriptingBlocks(lines);    
+    return commands;
+  },
+  
+  processScriptingBlocks: function(lines) {
+    // Processes the "```" blocks
+    return lines.reduce(function(o, s) {
+      if (o.scriptType) {
+        
+        // We're within a code block
+        
+        if (s.startsWith("```")) {
+          // It's the end of a code block
+          o.commands.push(Twison.processScriptingBlock(o.scriptType, o.scriptLines));
+          o.scriptLines.length = 0;
+        } else {
+          // A line within a code block
+          o.scriptLines.push(s);
+        }
+        
+      } else if (s.startsWith("```")) {
+        
+        // It's the start of a code block
+        
+        o.scriptType = s.slice(3).trimEnd() || 'js';
+        if (o.scriptType === 'js') {
+          o.scriptType = 'javascript';
+        }
+        
+      } else {        
+        // Plain text
+        o.commands.push(s);
+      }
+      return o;
+    }, {
+      commands: [],
+      scriptType: '',
+      scriptLines: []
+    })
+    .commands;
+  },
+  
+  processScriptingBlock: function(scriptType, lines) {
+    if (scriptType === 'javascript') {
+      try {
+        var compiledFunction = new Function('storage', lines.join('\n'));
+        return function monogataryCallWrapper() {
+          var storage = monogatari.storage();
+          var result = compiledFunction(storage);
+          monogatari.storage(storage);
+          return result;
+        }
+      } catch (e) {
+        console.error('Error while execute JS block. ', e);
+      }
+    } else {
+      // TODO: Proper error handling.
+      console.error('Unknown script type: ' + scriptType);
+    }
   },
 
   convertPassage: function(passage) {
